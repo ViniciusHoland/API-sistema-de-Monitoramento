ada4f924b44456c06c3f1261cca7382c4f13f8e1
ViniciusDev <viniholanda49@gmail.com>
2025-05-01 10:57:22 -0300

    created method loginUser and installed bcrypt and jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const userController = {
 
@@ -6,23 +7,70 @@ const userController = {
     registerUser: async (req, res) => {
         try {
 
-
-
-
             const { name, email, password } = req.body;
+
+            if (!name || !email || !password) {
+                return res.status(400).json({ message: "Please fill all the fields" });
+            }
+
+            const userExist = await User.findOne({email})
+
+            if (userExist) {
+                return res.status(400).json({ message: "User already exists" });
+            }
+
+            if (!name || !email || !password) {
+                return res.status(400).json({ message: "Please fill all the fields" });
+            }
+
+            if (password.length < 4) {
+                return res.status(400).json({ message: "Password must be at least 4 characters" });
+            }
+
+            const salts = 10
+
+            const hashPassword = await bcrypt.hash(password, salts)
 
             const user = await User.create({
                 name,
                 email,
-                password,
+                password: hashPassword
             });
 
-            res.status(201).json(user);
+            res.status(201).json({message: "User created successfully "+ user.name});
 
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     },
+
+    loginUser: async (req,res) => {
+
+        try{
+
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+
+            if(!user){
+                return res.status(400).json({ message: "User does not exist, please register" });
+            }
+
+            const hashPassword = await bcrypt.compare(password, user.password);
+
+            if(!hashPassword){
+                return res.status(400).json({ message: "Invalid credentials" });
+            }
+
+            res.status(200).json({ message: "Login successful" });
+
+
+        } catch(error){
+            res.status(500).json({ message: error.message });
+        }
+
+    }, 
+
 };
 
 export default userController;
