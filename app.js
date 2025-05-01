c123b4c14d88d4924c4e02fbf9189f3d6a25a295
ViniciusDev <viniholanda49@gmail.com>
2025-05-01 10:19:01 -0300

    created connection with database mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import conn from './database/conn.js';
+import router  from './routes/router.js';
+import cors from 'cors';
 
 const app = express()
+app.use(cors())
 dotenv.config()
+app.use(express.json())
 
+
+app.use('/api',router)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => {
+    
+    console.log(`Server running on port ${port}`) 
+    conn()
+})
